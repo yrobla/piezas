72365ac12d42c10c1f999f89b06a69fb0d58b5bd
Yolanda Robla <yolanda.robla@canonical.com>
2014-03-16 10:10:37 +0100

    workflow for adding pieces to search

--- piezas/apps/search/views.py
+++ piezas/apps/search/views.py
@@ -1,3 +1,5 @@
+from django.contrib import messages
+from django.utils.translation import ugettext_lazy as _
 from django.views.generic import CreateView
 from django.core.urlresolvers import reverse
 from oscar.core.loading import get_model
@@ -14,6 +16,9 @@ class HomeView(CreateView):
         # add that to basket
         basket = self.request.basket
         basket.add_product(form.instance, 1, None)
+
+        messages.success(self.request, _('Piece has been successfully added to search request'),
+                         extra_tags='safe noicon')
 
         return response
 
