47516ca4e30082f1a1c8a5176d95c5eb8fe85127
Yolanda Robla <yolanda.robla@canonical.com>
2014-03-21 17:59:52 +0100

    removing oscar

--- oscar/apps/checkout/signals.py
+++
@@ -1,5 +0,0 @@
-from django.dispatch import Signal
-
-pre_payment = Signal(providing_args=["view"])
-post_payment = Signal(providing_args=["view"])
-post_checkout = Signal(providing_args=["order", "user", "request", "response"])
