a78968ac78b5235669f03a1b0edf86d79c2ac096
Yolanda Robla <yolanda.robla-mota@hp.com>
2014-05-08 11:58:52 +0200

    changes in expiresearches

--- piezas/management/commands/expiresearches.py
+++ piezas/management/commands/expiresearches.py
@@ -1,11 +1,12 @@
+#!/usr/bin/env python
+import os
 from django.core.management.base import BaseCommand, CommandError
 from piezas import settings
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "piezas.settings")
 from piezas.apps.catalogue.models import SearchRequest
 from datetime import datetime, timedelta
 from django.utils.timezone import now
 
-class Command(BaseCommand):
-    def handle(self, *args, **options):
-        date_limit = now() - timedelta(seconds=5*settings.SEARCH_INTERVAL_MIN*60)
-        SearchRequest.objects.filter(date_created__lt=date_limit).update(state='expired')
+date_limit = now() - timedelta(seconds=5*settings.SEARCH_INTERVAL_MIN*60)
+SearchRequest.objects.filter(date_created__lt=date_limit).update(state='expired')
 
